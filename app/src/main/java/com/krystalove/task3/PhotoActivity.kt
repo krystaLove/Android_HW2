9482abeb54a0b154d624f9a53061e3ca412ac3c3
Денис <denis.zhuravkov@gmail.com>
2018-12-07 21:48:07 +0200

    --working

--- app/src/main/java/com/krystalove/task3/PhotoActivity.kt
+++ app/src/main/java/com/krystalove/task3/PhotoActivity.kt
@@ -4,8 +4,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_photo.*
 import android.graphics.BitmapFactory
-import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
 
 class PhotoActivity : AppCompatActivity() {
 
@@ -17,8 +15,7 @@ class PhotoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_photo)
 
         val image_file_location = intent.getStringExtra(IMAGE_LOCATION_TAG)
-        println(image_file_location)
-        Log.d("imageEE",image_file_location)
+
         val note_text = intent.getStringExtra(NOTE_TAG)
         val photo = BitmapFactory.decodeFile(image_file_location)
 
