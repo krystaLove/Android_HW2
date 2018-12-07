b7fa1974e57ac693463b92a1c00ede8c5036e552
Денис <denis.zhuravkov@gmail.com>
2018-12-07 21:44:24 +0200

    --working

--- app/src/main/java/com/krystalove/task3/MainActivity.kt
+++ app/src/main/java/com/krystalove/task3/MainActivity.kt
@@ -8,53 +8,78 @@ import android.os.Environment
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.*
 import android.support.v4.content.FileProvider
-import android.util.Log
-
+import android.app.Activity
+import android.support.design.widget.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CAPTURE_IMAGE = 200
+    private val IMAGE_LOCATION_TAG = "IMAGE"
+    private val NOTE_TAG = "NOTE"
+    private var image_file_location = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        camera.setOnClickListener{
-            openCameraIntent()
+        camera_btn.setOnClickListener{
+            if(!note_text.text.isEmpty()) openCameraIntent()
+            else showWarning(R.string.warning_message_1)
         }
     }
-
-    private val REQUEST_CAPTURE_IMAGE = 200
-    private var imagePath = ""
 
     private fun openCameraIntent() {
         val pictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if (pictureIntent.resolveActivity(packageManager) != null) {
             var photoFile: File? = null
-            try {
-                photoFile = createImageFile()
-            } catch (ex: IOException) {
-            }
+            try { photoFile = createImageFile() }
+            catch (ex: IOException) { showWarning(R.string.error_message_1) }
 
             if (photoFile != null) {
                 val authorities = BuildConfig.APPLICATION_ID + ".fileprovider"
                 val photoURI = FileProvider.getUriForFile(this, authorities, photoFile)
+
                 pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                 startActivityForResult(pictureIntent, REQUEST_CAPTURE_IMAGE)
-                Log.d("MyTag",photoFile.path)
             }
         }
     }
 
     @Throws(IOException::class)
     private fun createImageFile(): File {
-        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
-        val imageFileName = "IMG_" + timeStamp + "_"
         val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val image = File.createTempFile(imageFileName, ".jpg", storageDir)
-        imagePath = image.absolutePath
+        //val image = File.createTempFile("image", ".jpg", storageDir)
+        val image = File(storageDir?.path+"/image.jpg")
+        image_file_location = image.absolutePath
         return image
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        if (requestCode == REQUEST_CAPTURE_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                val photoActivityIntent = Intent(this, PhotoActivity::class.java)
+                photoActivityIntent.putExtra(IMAGE_LOCATION_TAG,image_file_location)
+                photoActivityIntent.putExtra(NOTE_TAG,note_text.text.toString())
+                startActivity(photoActivityIntent)
+                finish()
+            }
+            else showWarning(R.string.error_message_2)
+        }
+        super.onActivityResult(requestCode, resultCode, intent)
+    }
+    private fun showWarning(message_id:Int){
+        Snackbar.make(root_layout, message_id, Snackbar.LENGTH_LONG).show()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        image_file_location = savedInstanceState?.getString(IMAGE_LOCATION_TAG)!!
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putString(IMAGE_LOCATION_TAG, image_file_location)
     }
 
 }
--- app/src/main/java/com/krystalove/task3/PhotoActivity.kt
+++ app/src/main/java/com/krystalove/task3/PhotoActivity.kt
@@ -1,28 +1,29 @@
 package com.krystalove.task3
 
-import android.app.Activity
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.graphics.Bitmap
 import kotlinx.android.synthetic.main.activity_photo.*
 import android.graphics.BitmapFactory
-
-
-
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class PhotoActivity : AppCompatActivity() {
+
+    private val IMAGE_LOCATION_TAG = "IMAGE"
+    private val NOTE_TAG = "NOTE"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo)
 
-        /*var bmp: Bitmap? = null
-        val byteArray = intent.getByteArrayExtra("image")
-        bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+        val image_file_location = intent.getStringExtra(IMAGE_LOCATION_TAG)
+        println(image_file_location)
+        Log.d("imageEE",image_file_location)
+        val note_text = intent.getStringExtra(NOTE_TAG)
+        val photo = BitmapFactory.decodeFile(image_file_location)
 
-        photo_view.setImageBitmap(bmp)*/
-
+        photo_view.setImageBitmap(photo)
+        note.text = note_text.toString()
     }
 
 
